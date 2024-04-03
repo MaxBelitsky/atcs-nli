b5e18c90c471e57984c2a4c783340c29187e5b6d
Max Belitsky <md.belitsky@gmail.com>
2024-04-03 19:05:55 +0200

    Add structure to the training code

---
+++ src/train.py
@@ -0,0 +1,59 @@
+import argparse
+
+from src.models import LSTMEmbedder, BiLSTMEmbedder, BiLSTMPooledEmbedder, SentenceClassificationModel
+from src.utils import read_glove_embeddings, build_tokenizer, get_dataset
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Training sentence embedding models")
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        help="The model variant to train",
+        choices=["lstm"]
+    )
+    parser.add_argument(
+        "--mlp_hidden_dim",
+        type=int,
+        default=512,
+        help="The hidden dimension of the MLP head"
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=1e-5,
+        help="The learning rate"
+    )
+
+    args = parser.parse_args()
+
+    # Load GloVe embeddings
+    words, vectors = read_glove_embeddings()
+
+    # Load/build the tokenizer
+    tokenizer = build_tokenizer(words)
+
+    # Initialize the embedding model and auxiluary model
+    if args.model == "lstm":
+        embedder = LSTMEmbedder(vectors)
+    elif args.model == "bi-lstm":
+        embedder = BiLSTMEmbedder(vectors)
+    elif args.model == "bi-lstm-pool":
+        embedder = BiLSTMPooledEmbedder(vectors)
+
+    model = SentenceClassificationModel(embedder, args.mlp_hidden_dim, 3)
+
+    # Initialize the dataset
+    dataset = get_dataset()
+
+    # Set up logging
+
+    # Define training arguments
+    training_args = None
+
+    # Initialize trainer
+    trainer = None
+
+    trainer.train()
+    trainer.save_model()
