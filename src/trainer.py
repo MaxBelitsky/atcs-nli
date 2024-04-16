dc1f619c4b281c58c8e4e2bcd5d00808345b94fe
Max Belitsky <md.belitsky@gmail.com>
2024-04-16 22:09:12 +0200

    Update trainer

--- src/trainer.py
+++ src/trainer.py
@@ -1,9 +1,9 @@
 import os
 import logging
+from datetime import datetime
 
 import torch
 from torch.utils.data import DataLoader
-from torchmetrics import Accuracy
 from tqdm.auto import tqdm
 import wandb
 
@@ -27,14 +27,14 @@ class Trainer:
         # Create dataloaders
         collator = CustomCollator(tokenizer, device=args.device)
         self.train_dataloader = DataLoader(
-            dataset["train"],
+            self.train_data,
             collate_fn=collator,
             batch_size=args.batch_size,
             shuffle=True,
             pin_memory=True,
         )
         self.val_dataloader = DataLoader(
-            dataset["validation"],
+            self.val_data,
             collate_fn=collator,
             batch_size=args.batch_size,
             shuffle=True,
@@ -42,9 +42,9 @@ class Trainer:
         )
 
         # Initialize the optimizer
-        # self.optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
         self.optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=0.99)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=args.lr_decay)
+        self.scheduler_2 = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=args.lr_shrink)
 
         # Initialize a loss function
         self.loss_function = torch.nn.CrossEntropyLoss()
@@ -71,9 +71,8 @@ class Trainer:
 
     def train_model(self):
         best_val_accuracy = 0
-        for epoch in tqdm(range(self.args.epochs)):
+        for epoch in range(self.args.epochs):
             logger.info(f"Epoch: {epoch} | Learning rate: {self.scheduler.get_last_lr()}")
-            print(f"Epoch: {epoch} | Learning rate: {self.scheduler.get_last_lr()}")
             self.model.train()
             for batch in self.train_dataloader:
                 premises, hypotheses, labels = batch['premises'], batch['hypotheses'], batch['labels']
@@ -81,7 +80,7 @@ class Trainer:
                 self.optimizer.zero_grad()
 
                 # Forward pass
-                model_output = self.model(premises['input_ids'], hypotheses['input_ids'])
+                model_output = self.model(premises, hypotheses)
 
                 # Compute loss
                 loss = self.loss_function(model_output, labels)
@@ -99,12 +98,14 @@ class Trainer:
             val_metrics = self.evaluate_model(self.model, self.val_dataloader)
             if val_metrics['accuracy'] > best_val_accuracy:
                 best_val_accuracy = val_metrics['accuracy']
-                # TODO: save checkpoint
-                # TODO: scheduler step
-            self.log({"val": val_metrics})
-
+                self.save_model()
+                # Shrink learning rate if the accuracy improves
+                if epoch != 0:
+                    self.scheduler_2.step()
             # Update learning rate
             self.scheduler.step()
+
+            self.log({"val": val_metrics, "lr": self.scheduler.get_last_lr()})
 
     def evaluate_model(self, model, dataloader):
         model.eval()
@@ -112,7 +113,7 @@ class Trainer:
         for batch in dataloader:
             premises, hypotheses, labels = batch['premises'], batch['hypotheses'], batch['labels']
             with torch.no_grad():
-                model_output = model(premises['input_ids'], hypotheses['input_ids'])
+                model_output = model(premises, hypotheses)
             metrics = self.compute_metrics(model_output, labels)
             all_metrics.append(metrics)
         
@@ -138,7 +139,8 @@ class Trainer:
 
     def save_model(self):
         os.makedirs(self.args.output_dir, exist_ok=True)
-        model_file = os.path.join(self.args.output_dir, f"{self.args.model}.pt")
+        timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M")
+        model_file = os.path.join(self.args.output_dir, f"{self.args.model}_{timestamp}.pt")
         torch.save(self.model.state_dict(), model_file)
 
     def load_checkpoint_weights(self):
