0c4b78ce25b9876d3806487264a2e03635ffbdf0
Belitsky Max <scur0209@int5.local.snellius.surf.nl>
2024-04-22 19:59:04 +0200

    Add SLURM output

--- src/models.py
+++ src/models.py
@@ -13,11 +13,11 @@ class MeanEmbedder(nn.Module):
     def forward(self, x):
         embeddings = self.embedding(x['input_ids']) # [bs, tokens, embed_dim]
 
-        # out = embeddings.mean(dim=1) # [bs, embed_dim]
+        out = embeddings.mean(dim=1) # [bs, embed_dim]
 
         # Mask all unknown tokens so that 0s don't affect the mean
-        mask = embeddings.sum(dim=-1) != 0
-        out = embeddings.sum(dim=1) / mask.sum(dim=-1).view(embeddings.shape[0], 1)
+        # mask = embeddings.sum(dim=-1) != 0
+        # out = embeddings.sum(dim=1) / mask.sum(dim=-1).view(embeddings.shape[0], 1)
         return out
 
 
